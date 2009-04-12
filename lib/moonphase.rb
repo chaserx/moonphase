b2e473c43c64cc98a7e94549950cafc3cfb6ca9d
Chase Southard <chase.southard@gmail.com>
2009-04-11 23:59:14 -0400

    Added more information to the README, modified the postintsll and manifest files accordingly.

--- lib/moonphase.rb
+++ lib/moonphase.rb
@@ -5,7 +5,10 @@ require 'time'
 
 module Moonphase
   VERSION = '0.0.1'
-
+  # Moon
+  # Create a Moon object. <tt>my_moon = Moon.new</tt>
+  # Get the phase of the moon. <tt>my_moon.getphase("4/11")</tt>
+  # Put the moon phase to stdout <tt>puts my_moon.phase</tt>
   class Moon
     def initialize
       @phase = "unknown phase"
@@ -15,6 +18,7 @@ module Moonphase
     # getphase(arg)
     # Pass either a Time object like <tt>Time.now</tt> or a +String+ that +Time+ can parse like <tt>"4/11"</tt> or <tt>"Dec 31"</tt>
     def getphase(arg)
+      # accept only String or Time types
       if arg.class == String
         time = Time.parse(arg)
         date = time.strftime("%m %d %Y")
@@ -34,13 +38,13 @@ module Moonphase
     private
     # Calculates the phase of the moon. 
     # A string is passed to +calcphase+ from +getphase+. The attribute phase of class Moon is updated as a result. 
-    # Sections of this code were taken from the following:
+    # Sections of this code including calculation were taken from the following:
     # Python code by HAB
     # http://www.daniweb.com/code/snippet492.html
     # and translated into Ruby by Chase Southard on 4/4/2009
     def calcphase(arg)
       
-      #some constants to allow for the moon phase calculation
+      # some constants to allow for the moon phase calculation
       ages = [18, 0, 11, 22, 3, 14, 25, 6, 17, 28, 9, 20, 1, 12, 23, 4, 15, 26, 7]
       offsets = [-1, 1, 0, 1, 2, 3, 4, 5, 7, 7, 9, 9]
       description = ["new (totally dark)",
@@ -52,20 +56,23 @@ module Moonphase
           "in its last quarter (decreasing from full)",
           "waning crescent (decreasing from full)"]
       
-      #split the string argument into its components separated by spaces
+      # split the string argument into its components separated by spaces
       m, d, y = arg.split(/\s/)
+      # make them into integers for calculations
       month = m.to_i
       day = d.to_i
       year = y.to_i
       
+      # adjust day for a 30 calendar. I think.
       day = 1 if day == 31
-
+      
       days_into_phase = ((ages[(year + 1) % 19] + ((day + offsets[month-1]) % 30) + (if year < 1900 then 1 else 0 end)) % 30)
 
       index = ((days_into_phase + 2) * 16/59.0).to_i
 
       index = 7 if index > 7
       
+      # modify phase
       self.phase = description[index]
       
     end
